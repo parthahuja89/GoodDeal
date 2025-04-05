197538da79b8d97064bc81ffeec19acf8d7ef671
Parth Ahuja <parthahuja@Parths-MacBook-Air.local>
2025-04-05 15:46:09 -0700

    adding monokai theme

--- good-deal-ui/src/Components/Dashboard/dashboard.tsx
+++ good-deal-ui/src/Components/Dashboard/dashboard.tsx
@@ -51,30 +51,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <div className="flex justify-center ">
-      <Card className="bg-neutral-900 text-white border-neutral-800 w-3/5 h-[40vh] shadow-xl">
-        <CardContent>
-          <CardDescription className="mt-20 font-bold text-xl text-slate-200 flex gap-2 flex-col items-center justify-center">
-            Authenticating
-            <Button onClick={toggleTheme} className="mt-4">
-              {theme === "dark" ? (
-                <div className="flex items-center gap-2">
-                  <Sun className="w-4 h-4" /> Light Mode
-                </div>
-              ) : (
-                <div className="flex items-center gap-2">
-                  <Moon className="w-4 h-4" /> Dark Mode
-                </div>
-              )}
-            </Button>
-          </CardDescription>
-        </CardContent>
-      </Card>
-
-      <Button variant="secondary" onClick={() => setOpen(true)}>
-        {" "}
-        <Search />
-        Search (Cmd + K)
+    <div className="flex justify-between p-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <Button onClick={() => setOpen(true)} className="gap-2">
+        <Search size={12}/>
+        Search ({navigator.platform.includes("Mac") ? "⌘" : "Ctrl"} + K)
       </Button>
       
       <CommandDialog open={open} onOpenChange={setOpen}>
