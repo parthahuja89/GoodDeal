672d6da430ba31a3a33fb963733eb7cebf67d8c0
Parth Ahuja <parthahuj@gmail.com>
2025-05-17 20:59:50 -0700

    gitignore

--- .history/good-deal-backend/src/api/models/Game_20250512152053.ts
+++
@@ -1,5 +0,0 @@
-export interface Game {
-    id: string;
-    name: string;
-    asset_url: string;
-}
--- .history/good-deal-ui/src/models/GameDeal_20250512160126.ts
+++
@@ -1,9 +0,0 @@
-export interface GameDeal {
-    id: string;
-    asset_url: string;
-    title: string;
-    price_regular: number;
-    price_new: number;
-    url: string;
-}
-
