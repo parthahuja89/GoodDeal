8810f1ecd7beefb3aa11c91568d0928943d35d96
Parth Ahuja <parthahuj@gmail.com>
2025-06-13 20:05:15 -0700

    cookie issues

--- good-deal-backend/src/api/routeControllers/AuthController.ts
+++ good-deal-backend/src/api/routeControllers/AuthController.ts
@@ -23,6 +23,7 @@ route.post("/token", async (req: Request, res: Response) => {
     res.cookie('auth_token', token, {
       httpOnly: false,
       secure: false,
+      domain: process.env.NODE_ENV === 'production' ? 'good-deal-ui.vercel.app' : 'localhost',
       sameSite: 'lax',
     });
 
